8152101ffb586468299626281e587dd6fd21dbc7
imnotmoon <imnotmoon@gmail.com>
2020-08-14 17:10:19 +0900

    5
    5

--- hackathon/board/views.py
+++ hackathon/board/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from usedtrading.models import Usedtrading
+from bang.models import Bang
 
 # Create your views here.
 def board(request, category) :
     # category : secondhand || room
     print(category)
-    items = Usedtrading.objects.all()
+    if category == 'usedtrading':
+        items = Usedtrading.objects.all()
+    elif category == 'bang':
+        items = Bang.objects.all()
     return render(request, 'board.html', {'category' : category,
-                                            'items' : items})
+                                                'items' : items})
