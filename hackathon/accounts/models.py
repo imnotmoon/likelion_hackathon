0e7f3063c5566ad860dfdf79e4e78d5f02170583
jay <enlqn1010@gmail.com>
2020-08-14 12:24:27 +0900

    jay_version2

--- hackathon/accounts/models.py
+++ hackathon/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    fullname = models.TextField(max_length=10)
+    ssd = models.TextField(max_length=30)
 # Create your models here.
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
--- hackathon/accounts/views.py
+++ hackathon/accounts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth import login
+from .models import Profile
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request,user)
@@ -21,16 +24,13 @@ def logout(request):
     return redirect('home')
 
 def signup(request):
-    if request.method =='POST':
-        if request.POST['passwordconfig'] == request.POST['password']:
-            try:
-                user = User.objects.get(username=request.POST['username'])
-                return render(request,'signup.html',{'error':'유저이름이 존재합니다'})
-            except:
-                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password'])
-                auth.login(request,user)
-                return redirect('home')
-        else:
-            return render(request,'signup.html',{'error':'비밀번호가 다릅니다.'})
-    else:
-        return render(request,'signup.html')
+    if request.method == "POST":
+        if request.POST["password"] == request.POST["passwordconfig"]:
+            user = User.objects.create_user(username=request.POST["username"],password=request.POST["password"])
+            fullname = request.POST["fullname"]
+            ssd = request.POST['ssd']
+            profile = Profile(user=user, fullname=fullname,ssd=ssd)
+            profile.save()
+            auth.login(request,user)
+            return redirect('/')
+    return render(request, 'signup.html')
